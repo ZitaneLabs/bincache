eec5cede1eced0adab7f04af435b1ed133a953d4
SplittyDev <splittydev@gmail.com>
2023-06-13 02:15:00 +0200

    Export `MaybeCompressor` and `compression::Noop`

--- bincache/src/compression.rs
+++ bincache/src/compression.rs
@@ -1,11 +1,15 @@
 mod compression_level;
 mod maybe_compressor;
 mod noop_compressor;
+
+pub use compression_level::CompressionLevel;
+pub use maybe_compressor::MaybeCompressor;
+pub use noop_compressor::Noop;
+
+// zstd compression
+
 #[cfg(feature = "comp_zstd")]
 mod zstd_compressor;
 
-pub use compression_level::CompressionLevel;
-pub(crate) use maybe_compressor::MaybeCompressor;
-pub(crate) use noop_compressor::Noop;
 #[cfg(feature = "comp_zstd")]
 pub use zstd_compressor::Zstd;
