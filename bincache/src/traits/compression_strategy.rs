02d3104d5cdc63d6668ff5f7526ad44d366cfba1
SplittyDev <splittydev@gmail.com>
2023-06-14 16:51:49 +0200

    Implement `CompressionStrategy` for `Option<CompressionStrategy>`

--- bincache/src/traits/compression_strategy.rs
+++ bincache/src/traits/compression_strategy.rs
@@ -11,3 +11,20 @@ pub trait CompressionStrategy: std::fmt::Debug {
     /// Decompress binary data
     async fn decompress<'a>(&self, value: Cow<'a, [u8]>) -> Result<Cow<'a, [u8]>>;
 }
+
+#[async_trait]
+impl<T: CompressionStrategy + Sync + Send> CompressionStrategy for Option<T> {
+    async fn compress<'a>(&self, data: Cow<'a, [u8]>) -> Result<Cow<'a, [u8]>> {
+        match self {
+            Some(compressor) => compressor.compress(data).await,
+            None => Ok(data),
+        }
+    }
+
+    async fn decompress<'a>(&self, value: Cow<'a, [u8]>) -> Result<Cow<'a, [u8]>> {
+        match self {
+            Some(compressor) => compressor.decompress(value).await,
+            None => Ok(value),
+        }
+    }
+}
